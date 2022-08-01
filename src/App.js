ba1c098f0f2bf135e7efebbf23f093eaad4edf91
Zeerocious <zoro_z_99@hotmail.com>
2022-07-31 23:45:14 -0700

    Initializion completed. tested text modification and placements. enabled darkmode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@mui/material"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <box>
+      <Typography variant="header"> CHICKEN SANDWICH </Typography>
+      <Typography variant="item"> Classic Chicken Sandwich </Typography>
+      <Typography variant="description"> Our Delicious Crispy fried chicken sandwich, served with American Cheese, 2 Brioche Sliders, Pickles, Coleslaw, Spicy Mayo, and with Chef’s Quality Fries on the side </Typography>
+    </box>
   );
 }
 
 export default App;
+
+/*
+Contact us Section (no User input)                          
+We're Open! (no UI)
+Date/time Open (UI required)
+Telephon/Email (UI required)
+
+Date/time Open:
+
+What time is Monday open = (UI string 11AM)
+What time is Monday close = (UI string 10PM)
+What time is Tuesday open = (UI string 11AM)
+What time is Tuesday close = (UI string 10PM)
+What time is Wednesday open = (UI string 11AM)
+What time is Wednesday close = (UI string 10PM)
+What time is Thursday open = (UI string 11AM)
+What time is Thursday close = (UI string 10PM)
+What time is Friday open = (UI string 11AM)
+What time is Friday close = (UI string 10PM)
+What time is Saturday open = (UI string 11AM)
+What time is Saturday close = (UI string 12AM)
+What time is Sunday open = (UI string 11AM)
+What time is Sunday close = (UI string 12AM)
+
+Telephone/Email:
+
+What is the phone #: (UI string 510-709-9999)
+What is the email address: (UI string support@deliciousbytes.io)
+
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider, createTheme, CssBaseline, responsiveFontSizes } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+let theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    header: {
+      display: "block",
+      padding: 6,
+      fontWeight: 800,
+      fontSize: "2rem"
+    },
+    item: {
+      display: "block",
+      padding: 2,
+      fontWeight: 800,
+      fontSize: "1rem"
+    },
+    description: {
+      display: "block",
+      fontWeight: 300,
+      fontSize: "1rem"
+    }
+  }
+});
+theme = responsiveFontSizes(theme);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
